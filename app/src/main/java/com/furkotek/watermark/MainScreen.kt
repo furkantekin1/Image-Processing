4588550b3b3d6500a6ab10336edb025dff83da7c
ftekin <ftekin@cevizsoft.com>
2023-03-26 23:11:04 +0300

    Libraries added
    Awt kütüphane dosyaları ve gerekli jar dosyası eklendi.

--- app/src/main/java/com/furkotek/watermark/MainScreen.kt
+++ app/src/main/java/com/furkotek/watermark/MainScreen.kt
@@ -1,11 +1,60 @@
 package com.furkotek.watermark
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainScreen : AppCompatActivity() {
+
+    final val IMAGE_SELECT : Int = 100
+
+    lateinit var imgView : ImageView
+    lateinit var btnOpacity : Button
+    lateinit var btnResize : Button
+    lateinit var btnCompress : Button
+
+    var selectIntent : Intent = Intent();
+
+    fun init(){
+        imgView = findViewById(R.id.img_selected)
+        btnOpacity = findViewById(R.id.btn_opacity)
+        btnCompress = findViewById(R.id.btn_compress)
+        btnResize = findViewById(R.id.btn_resize)
+
+        selectIntent.setAction(Intent.ACTION_GET_CONTENT)
+        selectIntent.setType("image/*")
+        imgView.setOnClickListener(View.OnClickListener {
+            startActivityForResult(Intent.createChooser(selectIntent, "Select Image File"), IMAGE_SELECT)
+
+        })
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+        init()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(data != null){
+            var bitmap: Bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(data.data!!))
+            imgView.setImageBitmap(bitmap)
+            when (requestCode){
+                IMAGE_SELECT -> {
+
+                }
+            }
+        } else {
+            Toast.makeText(applicationContext, "Request Cancelled By User", Toast.LENGTH_SHORT).show()
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
---
+++ app/src/main/java/com/furkotek/watermark/awt/org/apache/harmony/awt/gl/NullTextRenderer.java
@@ -0,0 +1,19 @@
+package com.furkotek.watermark.awt.org.apache.harmony.awt.gl;
+
+import com.furkotek.watermark.awt.net.windward.android.awt.Graphics2D;
+import com.furkotek.watermark.awt.net.windward.android.awt.font.GlyphVector;
+
+public class NullTextRenderer extends TextRenderer {
+
+	@Override
+	public void drawString(Graphics2D g, String str, float x, float y) {
+		
+	}
+
+	@Override
+	public void drawGlyphVector(Graphics2D g, GlyphVector glyphVector, float x,
+			float y) {
+		
+	}
+
+}
